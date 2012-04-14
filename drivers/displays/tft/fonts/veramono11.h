f81ad0617826784582b729fc1c6459bb3cc4805d
Kevin Townsend <kevin@ktownsend.com>
2012-04-14 21:25:03 +0200

    Added fonts

---
+++ drivers/displays/tft/fonts/veramono11.h
@@ -0,0 +1,11 @@
+#ifndef __VERA_MONO_11__
+#define __VERA_MONO_11__
+
+#include "../fonts.h"
+
+/* Font data for Bitstream Vera Sans Mono 11pt */
+extern const uint8_t bitstreamVeraSansMono11ptCharBitmaps[];
+extern const FONT_CHAR_INFO bitstreamVeraSansMono11ptCharDescriptors[];
+extern const FONT_INFO bitstreamVeraSansMono11ptFontInfo;
+
+#endif
